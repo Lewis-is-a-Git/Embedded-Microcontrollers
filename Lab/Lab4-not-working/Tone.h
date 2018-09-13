352bfa733e1d9c464e72ad43300494707df19f62
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-09-13 13:44:04 +1000

    Create Tone.h

---
+++ Lab/Lab4-not-working/Tone.h
@@ -0,0 +1,14 @@
+#ifndef PROJECT_HEADERS_TONE_H_
+#define PROJECT_HEADERS_TONE_H_
+
+class Tone
+{
+private:
+	//variables
+public:
+	//functions
+	void toneSetup();
+	void toneLoop();
+};
+
+#endif /* PROJECT_HEADERS_TONE_H_ */
