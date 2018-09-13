684328d6884ccf917d046c47c237d3f56665c966
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-09-13 13:44:33 +1000

    Create Level.h

---
+++ Lab/Lab4-not-working/Level.h
@@ -0,0 +1,17 @@
+#ifndef PROJECT_HEADERS_LEVEL_H_
+#define PROJECT_HEADERS_LEVEL_H_
+
+class Level
+{
+private:
+	//variables
+public:
+	//functions
+	void drawCursor(int x, int y, int colour);
+	void levelSetup();
+	void levelLoop();
+};
+
+
+
+#endif /* PROJECT_HEADERS_LEVEL_H_ */
