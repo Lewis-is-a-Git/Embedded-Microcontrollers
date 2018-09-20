1ee59e5f995e8d00e9f2730c058b0fdd7f7214fd
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-09-20 17:58:07 +1000

    Create level.h

---
+++ Lab/4/level.h
@@ -0,0 +1,19 @@
+/*
+ * level.h
+ *
+ *  Created on: 20 Sep 2018
+ *      Author: root
+ */
+
+#ifndef SOURCES_LEVEL_H_
+#define SOURCES_LEVEL_H_
+
+
+void InitialiseLevel();
+
+#include "stdint.h"
+
+void drawCursorOnScreen(int16_t accelX, int16_t accelY);
+
+
+#endif /* SOURCES_LEVEL_H_ */
