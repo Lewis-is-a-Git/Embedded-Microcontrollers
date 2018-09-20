a387b73f7ad5594f7642e2cfe14443326c0cd62b
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-09-20 17:58:47 +1000

    Create tone.h

---
+++ Lab/4/tone.h
@@ -0,0 +1,34 @@
+/**
+ * Note.h
+ *
+ *  An interrupt driven system for playing
+ *  a single continuous note
+ */
+#ifndef SOURCES_NOTE_H_
+#define SOURCES_NOTE_H_
+
+/** Maximum note frequency */
+constexpr int NOTE_MAX = 20000;
+
+/** Minimum note frequency */
+constexpr int NOTE_MIN = 1000;
+
+/**
+ * Initialise the note playing system before first use
+ */
+void initialiseNotes(void);
+
+#include "stdint.h"
+int calculateFrequency(int16_t accelX, int16_t accelY);
+
+/**
+ * Changes the frequency of the note being played
+ *
+ * @param frequency - Frequency of note (100Hz-10kHz)
+ *
+ * @note This function returns immediately.
+ *       The note is played by interrupt driven code.
+ */
+void setNoteFrequency(unsigned frequency);
+
+#endif /* SOURCES_NOTE_H_ */
