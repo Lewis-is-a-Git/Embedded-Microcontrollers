4841b3c81fdd05879a070e5dc7b2399012aaccdb
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-10-03 16:57:31 +1000

    Update level.h

--- Lab/4/level.h
+++ Lab/4/level.h
@@ -1,19 +1,20 @@
 /*
  * level.h
- *
- *  Created on: 20 Sep 2018
- *      Author: root
+ * Interface for lCD display
  */
 
 #ifndef SOURCES_LEVEL_H_
 #define SOURCES_LEVEL_H_
+#include "stdint.h" //int16_t
 
-
+/*
+ * Initailise the LCD level Display
+ */
 void InitialiseLevel();
 
-#include "stdint.h"
-
+/*
+ * Draw a cursor on the screen
+ */
 void drawCursorOnScreen(int16_t accelX, int16_t accelY);
-
 
 #endif /* SOURCES_LEVEL_H_ */
