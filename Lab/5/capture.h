685e3456dcf98ef7ed549aae9467eacebe64ca47
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-10-13 14:47:21 +1100

    Create capture.h

---
+++ Lab/5/capture.h
@@ -0,0 +1,15 @@
+/*
+ * capture.h
+ *
+ *  Created on: 12 Oct 2018
+ *      Author: root
+ */
+
+#ifndef SOURCES_CAPTURE_H_
+#define SOURCES_CAPTURE_H_
+
+void captureInit();
+
+void startStopPit(bool start, int slowness);
+
+#endif /* SOURCES_CAPTURE_H_ */
