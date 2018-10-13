9c12a892f08e1bed9837a2b7c24d1bb5b7a3f4a4
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-10-13 14:45:38 +1100

    Update main.cpp

--- Lab/5/main.cpp
+++ Lab/5/main.cpp
@@ -1,24 +1,22 @@
-/*
- ============================================================================
- * @file    main.cpp (180.ARM_Peripherals/Sources/main.cpp)
- * @brief   Basic C++ demo
- *
- *  Created on: 10/1/2016
- *      Author: podonoghue
- ============================================================================
- */
 #include "hardware.h"
-#include "screen.h"
+#include "pit.h"
+#include "lcd.h"
+
+#include "screen.h" //display grid and menu
+#include "capture.h" //capture values form adc using pit
+#include "menu.h"  //joystick menu
+
 // Allow access to USBDM methods without USBDM:: prefix
 using namespace USBDM;
 
 int main() {
+	captureInit();
 	displayInit();
 
-   for(;;) {
+	joystickInit();
 
-      drawGrid();
-      waitMS(100);
-   }
-   return 0;
+	for(;;) {
+		readJoystick();
+	}
 }
+
