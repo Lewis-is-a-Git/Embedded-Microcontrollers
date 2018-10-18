75fa950279b8cd38a83e7c9a72cfa64d6c572db4
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-10-18 18:09:22 +1100

    Update capture.cpp

--- Lab/5/capture.cpp
+++ Lab/5/capture.cpp
@@ -1,97 +1,142 @@
 /*
  * capture.cpp
  *
- *  Created on: 12 Oct 2018
- *      Author: root
+ *  Created on: 17 Oct 2018
+ *      Author: FAHAD
  */
-#include "hardware.h"
-#include "pit.h"
 
 #include "capture.h"
-#include "screen.h"
+#include "hardware.h"
+#include "display.h"
+#include "pit.h"
+#include "stdint.h"
 
 using namespace USBDM;
 
+// Connection mapping - change as required
 using adc = Adc0;
-using potChannel = Adc0Channel<12>;
-using ground = GpioB<3>;
+
+// Jog switch on LCD board
+using EastSwitch   = USBDM::GpioC<0,ActiveLow>;
+using SouthSwitch  = USBDM::GpioC<1,ActiveLow>;
+using WestSwitch   = USBDM::GpioD<6,ActiveLow>;
+using CentreSwitch = USBDM::GpioD<5,ActiveLow>;
+using NorthSwitch  = USBDM::GpioB<1,ActiveLow>;
+
+using potchannel = Adc0Channel<12>;
+
+using vdd = GpioA<13>;
+using ground = GpioC<10>;
 
 using timer = Pit;
 using timerChannel = PitChannel<0>;
 
-//GLobal variables to communicate to callback function
-uint8_t data[100];
-int dataCounter=0;
+int dataCounter = 0;
+uint8_t data[DATA_POINTS];
 
-/**
- * Start capture of data values at a fixed rate.
- * This routine may return before the data
- * capture is complete.
- *
- * @param[in] rate Sample rate (1 to 500 ms)
- * @param[out] data Buffer for data
- *
- */
-void capture(int rate, uint8_t data[DATA_POINTS]){
-	data[dataCounter] = potChannel::readAnalogue();
-	timerChannel::configure(rate*ms, PitChannelIrq_Enable);
+static constexpr unsigned MAX_DATA_VALUE = 110;
+
+void joystickInit(){
+
+	EastSwitch::setInput(PinPull_Up, PinAction_None, PinFilter_Passive);
+	SouthSwitch::setInput(PinPull_Up, PinAction_None, PinFilter_Passive);
+	WestSwitch::setInput(PinPull_Up, PinAction_None, PinFilter_Passive);
+	CentreSwitch::setInput(PinPull_Up, PinAction_None, PinFilter_Passive);
+	NorthSwitch::setInput(PinPull_Up, PinAction_None, PinFilter_Passive);
 }
-/**
- * Checks if capture is complete
- *
- * @return true => capture complete
- * @return false => capture busy
- */
-bool isCaptureComplete(){
-	return (dataCounter == 100);
-}
-/**
- * PIT callback function
- * captures data from the adc
- * plots the data on the screen
- * Increments data counter global variable
- */
-void pitCallback(void){
 
-	capture(100, data);
-	drawData(data);
+void readJoyStick(){
+	static int menuChoiceIndex = 0;
+	int rates[] = {1, 2, 5, 10, 20, 50, 100, 200, 500};
 
-	dataCounter++;
-	if (isCaptureComplete()){
-		dataCounter = 0;
-		displayInit();
+	int currentSwitchValue =
+			(EastSwitch::read()<<4)|
+			(SouthSwitch::read()<<3)|
+			(WestSwitch::read()<<2)|
+			(CentreSwitch::read()<<1)|
+			(NorthSwitch::read()<<0);
+
+	int oldSwitchValue;
+	//up = 1
+	//in = 2
+	//left = 4
+	//down = 8
+	//right = 16
+
+	//move up
+	if (currentSwitchValue == 1 && oldSwitchValue == 0){
+		menuChoiceIndex++;
+		if (menuChoiceIndex > 8){
+			menuChoiceIndex = 8;
+		}
+		drawMenu(menuChoiceIndex);
 	}
+	//move down
+	if (currentSwitchValue == 8 && oldSwitchValue == 0){
+		menuChoiceIndex--;
+		if (menuChoiceIndex < 0){
+			menuChoiceIndex = 0;
+		}
+		drawMenu(menuChoiceIndex);
+	}
+	if (currentSwitchValue == 2){
+		captureInit();
+		capture(rates[menuChoiceIndex], data);
+	}
+
+	oldSwitchValue = currentSwitchValue;
 }
-/**
- * Initialise the capture of the adc values using the PIT as a timer
- */
-void captureInit(){
-	//joystick needs 8bit (256) screen uses 12bit (4096)
-	adc::configure(AdcResolution_8bit_se, AdcClockSource_Bus, AdcClockDivider_2);
+
+void capture(int rate, uint8_t array[DATA_POINTS]){
+	timerChannel::configure(rate*ms, PitChannelIrq_Enable);
+	timerChannel::enableNvicInterrupts(true, NvicPriority_Normal);
+}
+
+bool isCaptureComplete(){
+	return (dataCounter > 98);
+}
+
+
+void PitCallback(void){
+	adc::enableNvicInterrupts(true, NvicPriority_Normal);
+	potchannel::startConversion(AdcInterrupt_enable);
+}
+void adcCallback(uint32_t value, int channel){
+	DrawSampling();
+	data[dataCounter] = value*5;
+	if (data[dataCounter] > MAX_DATA_VALUE){
+		data[dataCounter] = MAX_DATA_VALUE;
+	}
+	console.write("data value = ").writeln(data[dataCounter]);
+	dataCounter++;
+	adc::enableNvicInterrupts(false, NvicPriority_Normal);
+	if (isCaptureComplete()){
+		timerChannel::enableNvicInterrupts(false, NvicPriority_Normal);
+	}
+
+}
+
+void  captureInit(){
+	adc::configure(AdcResolution_12bit_se, AdcClockSource_Bus, AdcClockDivider_2 );
 	adc::calibrate();
-	adc::setAveraging(AdcAveraging_32);
+	adc::setCallback(adcCallback);
+	potchannel::setInput();
 
-	potChannel::setInput();
+	potchannel::startConversion(AdcInterrupt_enable);
 
-	//simulate ground pin, because of access
+	vdd::setOut();
+	vdd::write(1);
+
 	ground::setOutput();
 	ground::write(0);
 
-	timer::configure();
-
-	timerChannel::setCallback(pitCallback);
-	timerChannel::configure(100*ms, PitChannelIrq_Enable);
-	timerChannel::enableNvicInterrupts(true, NvicPriority_Normal);
+	timer::configure(PitDebugMode_Stop);
+	timerChannel::setCallback(PitCallback);
+	checkError();
 }
-/**
- * used to pause the data capture while on the menu
- */
-void startStopPit(bool start, int slowness){
-	if (start){
-		timerChannel::configure(slowness*ms, PitChannelIrq_Enable);
-	}
-	if (!start){
-		timerChannel::configure(slowness*ms, PitChannelIrq_Disable);
-	}
+void displayData(){
+	displayInit();
+	drawGrid();
+	drawData(data);
 }
 
