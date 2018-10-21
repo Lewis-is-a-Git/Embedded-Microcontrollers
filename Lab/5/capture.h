295eeb54681d67a3971566dcd5d5a0f2e3c15cc9
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-10-21 11:12:59 +1100

    Update capture.h

--- Lab/5/capture.h
+++ Lab/5/capture.h
@@ -1,8 +1,5 @@
 /*
  * capture.h
- *
- *  Created on: 17 Oct 2018
- *      Author: FAHAD
  */
 
 #ifndef SOURCES_CAPTURE_H_
@@ -12,29 +9,41 @@
 #include "display.h"
 
 /**
-* Start capture of data values at a fixed rate.
-* This routine may return before the data
-* capture is complete.
-*
-* @param[in] rate Sample rate (1 to 500 ms)
-* @param[out] data Buffer for data
-*
-*/
-void capture(int rate, uint8_t array[DATA_POINTS]);
-/**
-* Checks if capture is complete
-*
-* @return true => capture complete
-* @return false => capture busy
-*/
-bool isCaptureComplete();
-
-void captureInit();
-
+ * Initailise the joystick controls
+ */
 void joystickInit();
 
+/**
+ * Read Joystick and Control Menu Selection
+ */
 void readJoyStick();
 
+/**
+ * Start capture of data values at a fixed rate.
+ * This routine may return before the data
+ * capture is complete.
+ *
+ * @param[in] rate Sample rate (1 to 500 ms)
+ * @param[out] data Buffer for data
+ *
+ */
+void capture(int rate, uint8_t array[DATA_POINTS]);
+/**
+ * Checks if capture is complete
+ *
+ * @return true => capture complete
+ * @return false => capture busy
+ */
+bool isCaptureComplete();
+
+/**
+ * Initialise the ADC and PIT to capture data
+ */
+void captureInit();
+
+/**
+ * Displays the Data after capture is complete
+ */
 void displayData();
 
 #endif /* SOURCES_CAPTURE_H_ */
