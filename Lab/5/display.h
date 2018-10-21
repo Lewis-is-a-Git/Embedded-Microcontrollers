7a0230240b17935c6ecaa9b8e204b2889d0be9c8
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-10-21 11:14:05 +1100

    Update display.h

--- Lab/5/display.h
+++ Lab/5/display.h
@@ -1,38 +1,39 @@
 /*
  * display.h
- *
- *  Created on: 17 Oct 2018
- *      Author: FAHAD
  */
-
 #ifndef SOURCES_DISPLAY_H_
 #define SOURCES_DISPLAY_H_
-#include "stdint.h"
+#include "stdint.h" //uint8_t
 
 /** Number of data points to capture - Adjust as needed */
 constexpr unsigned DATA_POINTS = 100;
 
-
 /**
-* Draws data on screen (as fast as possible!)
-*
-* @param data - Array of data to plot
-*/
-void drawData(uint8_t array[DATA_POINTS]);
-/**
-* Draw grid and axis labels for display
-*/
-void drawGrid();
-/**
-* Initialise LCD
-*/
-
-void drawMenu(int menuChoice);
-
+ * Initalise the display, clears the screen and set the font
+ */
 void displayInit();
 
+/**
+ * Draws the menu on the screen
+ * @param[in] menuChoice - menu item index
+ */
+void drawMenu(int menuChoice);
 
-void DrawSampling();
+/**
+ * Draws data on screen (as fast as possible!)
+ *
+ * @param data - Array of data to plot
+ */
+void drawData(uint8_t array[DATA_POINTS]);
 
+/**
+ * Draw grid and axis labels for display
+ */
+void drawGrid();
+
+/**
+ * Draw a sampling animation
+ */
+void drawSampling();
 
 #endif /* SOURCES_DISPLAY_H_ */
